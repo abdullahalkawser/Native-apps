4744e9e41c56aafd700abc4f4475515add3afab5
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-10-12 12:03:17 +0600

    add new word

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import "./global.css"
 export default function App() {
   return (
     <View  className='bg-red-600 mt-20'>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! this is my new app</Text>
       <StatusBar style="auto" />
     </View>
   );
